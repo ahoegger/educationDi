319c5a4666c93c964a04a93456473ce892289183
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2014-09-02 15:25:01 +0200

    remove simple di example

--- com.bsiag.education.di.simple/src/com/bsiag/education/di/simple/Inject.java
+++
@@ -1,9 +0,0 @@
-package com.bsiag.education.di.simple;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Inject {
-
-}
